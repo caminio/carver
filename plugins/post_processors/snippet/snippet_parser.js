768dc7ae6e74cab844310fe01bd48eb10d2917ad
David Reinisch <david.reinisch@tastenwerk.com>
2014-06-11 00:00:57 +0200

    working with snippet array

--- plugins/post_processors/snippet/snippet_parser.js
+++ plugins/post_processors/snippet/snippet_parser.js
@@ -7,7 +7,7 @@
  * @Date:   2014-06-06 17:09:41
  *
  * @Last Modified by:   David Reinisch
- * @Last Modified time: 2014-06-10 14:17:05
+ * @Last Modified time: 2014-06-11 00:00:26
  *
  * This source code is not part of the public domain
  * If server side nodejs, it is intendet to be read by
@@ -19,17 +19,9 @@ module.exports = function ( Carver ) {
 
   'use strict';
 
-  var snippetRegexp;
-  var contentPath;
-
-  var globalContent;
-
-  var join = require('path').join;
-  var async = require('async');
-  
-  var _ = require('lodash');
+  var _          = require('lodash');
+  var join       = require('path').join;
   var inflection = require('inflection');
-  var fs = require('fs');
 
   /**
    *  @constructor
@@ -39,88 +31,35 @@ module.exports = function ( Carver ) {
    */
   function SnippetParser( content, compiler, resolve ){
 
-   // console.log('calling with: ', content );
+    var contentPath = compiler.options.cwd;
+    var keyword = compiler.options.keyword || 'snippet';
+    var snippets = getSnippets( content, keyword, contentPath );
 
-    contentPath = compiler.options.cwd;
-    snippetRegexp = buildSnippetRegexp();
+    getSnippetsContent( snippets, keyword, compiler.options );
 
-    var snippets = getSnippets( content );
-
-    snippets = getContent( snippets, 'pebbles', compiler.options );
-
-    var compile = runCompiler( compiler );
-    globalContent = content;
-
-    async.eachSeries( snippets, compile, function(){
-      // console.log('output: ', globalContent );
-      resolve( globalContent );
-    });
-
-  }
+    var runner = require('./snippet_runner')(compiler, resolve);
+    runner.run( snippets, content );
+  } 
 
   /**
-   *  @method runCompiler
-   *  @param compiler { Object }
-   *  @return { Function }
-   */
-  function runCompiler( compiler ){
-    /**
-     *  @param snippet
-     *  @param nextSnippet
-     */
-    return function( snippet, nextSnippet ){
-      var items = snippet.params.array ? snippet.params.array : [ snippet.content ];
-      var localContent = '';
-      var index = 0;
-
-      async.each( items, function( item, nextItem ){
-
-        if( typeof item !== 'string' ){
-          item = getTranslation( item.translations, compiler.options.lang );
-          var arrayName =  snippet.params.name || inflection.singularize( snippet.params.array );
-          item.index = index;
-          compiler.locals[ arrayName ] = item;
-          index++;
-        }
- 
-        if( fs.existsSync( join( snippet.path, snippet.name + '.jade' ))){
-          console.log('path: ', snippet.path, 'template: ', snippet.name );
-          compiler
-            .set('cwd',snippet.path ).set('template', snippet.name );
-        }
-
-        compiler
-          .registerEngine('jade', require('jade'))         
-          .render( item )
-          .then( function( html ){ localContent += html; console.log('WE GET: ', html ); nextItem(); } ); 
-      }, function(){
-        globalContent = globalContent.replace( snippet.original, localContent );
-        nextSnippet();
-      });
-    };
-  }
-
-  /**
-   *  @method getContent
+   *  @method getSnippetsContent
    *  @param snippets { Array }
    *  @param keyword { String }
    *  @param options { Object }
    */
-  function getContent( snippets, keyword, options ){
-    
+  function getSnippetsContent( snippets, keyword, options ){
     var curLang = options.lang;
     var locals = options.locals;
+    var dbKeyword = inflection.pluralize( keyword );
 
-    if( !locals.doc || !locals.doc[keyword] )
-      throw new Error('no keyword');
-
-    snippets.forEach( function( curSnippet ){
-      var data = _.find( locals.doc[keyword], { 'name': curSnippet.name });
-      if( data )
-        curSnippet.content = getTranslation( data.translations, curLang );
-      else
-        curSnippet.content = '{{ ' + curSnippet.name + ': NO DATA FOUND }}';
-    });
+    if( locals.doc && locals.doc[dbKeyword] )
+      snippets.forEach( function( curSnippet ){
+        var data = _.find( locals.doc[dbKeyword], { 'name': curSnippet.name });
+        if( data )
+          curSnippet.content = getTranslation( data.translations, curLang );
+        else
+          curSnippet.content = '{{ ' + curSnippet.name + ': NO DATA FOUND }}';
+      });
 
     return snippets;
   }
@@ -136,8 +75,7 @@ module.exports = function ( Carver ) {
     return _.find( translations, { 'locale': curLang }).content; 
   }
 
-  function buildSnippetRegexp( alternativKeyword ){
-    var keyword = 'snippet' || alternativKeyword ;
+  function buildSnippetRegexp( keyword ){
     return new RegExp( '^{{\\s*' + keyword + '(:|( :))', 'gi' );
   }
 
@@ -145,15 +83,15 @@ module.exports = function ( Carver ) {
    *  @method getSnippets
    *  @param contetn { String }
    */
-  function getSnippets( content ){
+  function getSnippets( content, keyword, path ){
     var originalStrings = content.match(/{{[^{}]*}}/g);
     var snippets = [];
 
     if( originalStrings !== null )
       originalStrings.forEach( function( original ){
-        var isSnippet = original.match( snippetRegexp );
+        var isSnippet = original.match( buildSnippetRegexp( keyword ) );
         if( isSnippet instanceof Array )
-          snippets.push( toSnippetObject( original ));
+          snippets.push( toSnippetObject( original, path, keyword ));
         else 
           console.log( 'IS NO VALID SNIPPET, TODO' );
       });
@@ -167,7 +105,7 @@ module.exports = function ( Carver ) {
    *  @param originalSnippet { String }
    *  @return { Object }
    */
-  function toSnippetObject( original ){
+  function toSnippetObject( original, path, keyword ){
     var snippet = {
       original: original
     };
@@ -177,7 +115,7 @@ module.exports = function ( Carver ) {
     snippet.name = replaceAll(" ", "", params[0].split(':')[1] );
     params.shift();
     snippet.params = splitKeyValueArray( params );
-    snippet.path = join( contentPath, 'snippets', snippet.name );
+    snippet.path = join( path, keyword, snippet.name );
 
     return snippet;
   }
