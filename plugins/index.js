90085b8c0d1266c562fb8ae1d16613f9dd1cfabb
David Reinisch <david.reinisch@tastenwerk.com>
2014-06-11 18:05:51 +0200

    kicked Carver from wrapper

---
+++ plugins/index.js
@@ -0,0 +1,20 @@
+/** 
+ * @Author: David Reinisch
+ * @Company: TASTENWERK e.U.
+ * @Copyright: 2014 by TASTENWERK
+ * @License: Commercial
+ *
+ * @Date:   2014-06-11 17:56:54
+ *
+ * @Last Modified by:   David Reinisch
+ * @Last Modified time: 2014-06-11 18:02:08
+ *
+ * This source code is not part of the public domain
+ * If server side nodejs, it is intendet to be read by
+ * authorized staff, collaborator or legal partner of
+ * TASTENWERK only
+ */
+
+module.exports = {
+  snippet_parser: require('./pre_processors/snippet/snippet_parser')
+};
